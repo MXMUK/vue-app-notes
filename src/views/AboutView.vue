fe86b0477ea9c9a6d5331c11596a2bab4a751134
Roman Maksymuk <romamaksymuk@gmail.com>
2023-02-20 17:08:50 +0200

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, iure, ducimus, dolorum laborum cupiditate mollitia voluptas voluptatum provident laudantium a esse quo quia laboriosam magnam. Est numquam earum aut facilis.</h1>
   </div>
 </template>
 
