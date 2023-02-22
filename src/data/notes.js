f46a0fe9bb76f6c38370ca4f75613476e2a57cf7
Roman Maksymuk <romamaksymuk@gmail.com>
2023-02-22 18:41:06 +0200

    all implemented

---
+++ src/data/notes.js
@@ -0,0 +1,35 @@
+export default [
+  { 
+    id: 1, 
+    title: 'In this section we will introduce how to scaffold a Vue Single Page Application on your local machine. The created project will be using a build setup based on', 
+    todos: [{
+      id: 1,
+      completed: false,
+      name: 'implement app',
+    }] 
+  },
+  { 
+    id: 2, 
+    title: 'Here we are using unpkg, but you can also use any CDN that serves npm packages, for example jsdelivr or cdnjs. Of course, you can also download this file and serve it yourself.', 
+    todos: [{
+      id: 1,
+      completed: true,
+      name: 'implement app 2',
+    },
+    {
+      id: 2,
+      completed: false,
+      name: 'implement app 3',
+    },
+    {
+      id: 3,
+      completed: false,
+      name: 'implement app 4',
+    },
+    {
+      id: 4,
+      completed: false,
+      name: 'implement app 5',
+    }] 
+  },
+]
--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './assets/styles/index.scss';
 
 const app = createApp(App)
 
